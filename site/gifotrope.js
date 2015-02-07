9939d6e8bf5c1d702cc256ec12e21b60231219fa
Oliver Wise <owise1@gmail.com>
2015-02-06 17:09:54 -0800

    use stylesheets instead of modifying elements individually

--- site/gifotrope.js
+++ site/gifotrope.js
@@ -6,55 +6,96 @@
  * called frames. Or pass it in in the options.
  */
 (function( $ ) {
+  var imagesWithStyles = [];
+
+  // set up stylesheet
+  var style = document.createElement('style');
+  style.appendChild(document.createTextNode(""));
+  document.head.appendChild(style);
+
+  function addRule(selector, rules, index) {
+    if("insertRule" in style.sheet) {
+      style.sheet.insertRule(selector + "{" + rules + "}", index);
+    } else if("addRule" in style.sheet) {
+      style.sheet.addRule(selector, rules, index);
+    }
+  }
+
   $.fn.gifotrope = function(o) {
     var opts = $.extend( {
-      slowDown : 10
+      slow : 100
     }, o);
 
 	
-	return this.each(function(){
-		var that = this;
-    var image, matches, width;
+    return this.each(function(){
+      var that = this;
+      var image, matches, width;
 
-    // images bg
-    if(opts.image){
-      $(this).css('background', 'url('+opts.image+')');
-      image = opts.image;
-    } else {
-      matches = $(this).css('background-image').match(/url\(([^)]+)\)/i);
-      if(matches){
-        image = matches[1].replace(/["']/g, '');
+      // images bg
+      if(opts.image){
+        $(this).css('background', 'url('+opts.image+')');
+        image = opts.image;
+      } else {
+        matches = $(this).css('background-image').match(/url\(([^)]+)\)/i);
+        if(matches){
+          image = matches[1].replace(/["']/g, '');
+        }
       }
-    }
+      if(!image) return console.log("gifotrope: couldn't find image");
 
-    // frames
-    var frames  = opts.frames || $(this).data('frames');
-    matches = $(this).css('background-image').match(/url\(.*-(\d+)\.(png|jpg|jpeg|gif)["']*\)/i);
-    if(matches && !frames){
-      frames = matches[1];
-    }
-    frames = parseInt(frames, 10);
-    
-    if(image && frames){
-      var i    = new Image;
-      i.src    = image;
-      i.onload = function(){
-        width  = this.width / frames;
+      var imageIndex = imagesWithStyles.indexOf(image);
+      if(imageIndex < 0){
+        
+          // how many frames?
+          var frames  = opts.frames || $(this).data('frames');
+          matches = $(this).css('background-image').match(/url\(.*-(\d+)\.(png|jpg|jpeg|gif)["']*\)/i);
+          if(matches && !frames){
+            frames = matches[1];
+          }
+          frames = parseInt(frames, 10);
+          if(!frames) return console.log("gifotrope: couldn't determine frames");
 
-        $(window).scroll(function(){
-          $(that).css('background-position-x', width * Math.floor(($(window).scrollTop() % width) / opts.slowDown));
-        });
+          var frameRange = [];
+          var z = 0;
+          while(frameRange.push(z++)<frames);
+
+          imagesWithStyles.push({
+            image : image,
+            frames : frames
+          });
+          imageIndex = imagesWithStyles.length - 1;
+
+          // create styles
+          var myClass = 'gifotrope-' + imageIndex;
+          addRule('.' + myClass, "background-image: url("+image+")");
+          
+          var i    = new Image;
+          i.src    = image;
+          i.onload = function(){
+            width  = this.width / frames;
+
+            frameRange.forEach(function(z){
+              addRule('.' + myClass + '.f-' + z, "background-position-x: "+width*z+"px");
+            });
+          }
+          $(window).scroll(function(){
+            var f = Math.floor($(window).scrollTop() / opts.slow) % frames;
+            if(f < 0) return;
+            $('.' + myClass)
+              .removeClass(frameRange.map(function(z){ return 'f-' + z; }).join(' '))
+              .addClass('f-' + f);
+          });
       }
-    }
-			
-			
-		var methods = {
-		}
-		
-		
-			
-		
-	});
+      
+      // add the classes
+      $(this).addClass('gifotrope-' + imageIndex);
 
+        
+        
+      var methods = {
+      }
+		
+		
+    });
   };
 })( jQuery );
