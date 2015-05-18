0f5d636c1f8cb090e72e875d13f653f548c26234
Oliver Wise <owise1@gmail.com>
2015-05-18 16:45:47 -0700

    fixed paths

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ module.exports = function(imagePath, cb){
   var addRand   = function(f){ return rand + '/' + f; };
 
   fs.mkdir(rand, function(err, ok){
-    exec('cp ' + imagePath + ' ' + __dirname + '/' + rand + '; cd ' + __dirname + '/' + rand + '; gif2anim -c ' + path.basename(imagePath), function(error, stdout, stderr) {
+    exec('cp ' + imagePath + ' ' + __dirname + '/' + rand + '; cd ' + __dirname + '/' + rand + '; ' + __dirname + '/gif2anim -c ' + path.basename(imagePath), function(error, stdout, stderr) {
       if (error !== null) {
           return console.log('exec error: ' + error);
       }
@@ -25,7 +25,7 @@ module.exports = function(imagePath, cb){
         });
         var theFile =  originalDir + '/' + path.basename(imagePath, '.gif') + '-' + files.length + '.png';
         strip.write(theFile, function(err){
-          allFiles.map(addRand).map(fs.unlink);
+          allFiles.map(addRand).map(function (f) { return fs.unlink(f) });
           fs.rmdir('./' + rand);
           if(cb) cb(theFile);
         });
